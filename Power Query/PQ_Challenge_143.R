eebbce94382a503f9c29bb82e33f61f8e9a10f95
kgryczan <kgryczan@gmail.com>
2023-12-30 10:24:01 +0100

    PQ 143 Uploaded

---
+++ Power Query/PQ_Challenge_143.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(readxl)
+
+input = read_excel("Power Query/PQ_Challenge_143.xlsx", range = "A1:C21")
+test  = read_excel("Power Query/PQ_Challenge_143.xlsx", range = "F1:H7")
+
+result = input %>%
+  group_by(Emp, Value) %>%
+  mutate(rn = row_number()) %>%
+  filter(rn == 2 | (rn == max(rn) & rn > 2)) %>%
+  select(-rn) %>%
+  ungroup()
+
+identical(result, test)
+#> [1] TRUE
+

f1549d940b4a6c3c3fd978f924a880c5e50e0498
kgryczan <kgryczan@gmail.com>
2024-01-30 06:07:38 +0100

    Exc 380 Uploaded

---
+++ Excel/380 Challenge.R
@@ -0,0 +1,34 @@
+library(tidyverse)
+library(readxl)
+
+test1 = read_excel("Excel/380 Draw NxN Squares.xlsx", range = "A2:H9", col_names = FALSE) 
+colnames(test1) = NULL
+test2 = read_excel("Excel/380 Draw NxN Squares.xlsx", range = "A11:G17", col_names = FALSE)
+colnames(test2) = NULL
+test3 = read_excel("Excel/380 Draw NxN Squares.xlsx", range = "A19:E23", col_names = FALSE)
+colnames(test3) = NULL
+test4 = read_excel("Excel/380 Draw NxN Squares.xlsx", range = "A25:D28", col_names = FALSE)
+colnames(test4) = NULL
+
+draw_sides_and_diag = function(matrix_size) {
+  mat = matrix(NA, nrow = matrix_size, ncol = matrix_size)
+  mat[1,] = "x"
+  mat[matrix_size,] = "x"
+  mat[,1] = "x"
+  mat[,matrix_size] = "x"
+  diag(mat) = "x"
+  diag(mat[,ncol(mat):1]) = "x"
+  mat = as_tibble(mat)
+  colnames(mat) = NULL
+  return(mat)
+}
+
+all.equal(draw_sides_and_diag(8), test1)
+#> [1] TRUE
+all.equal(draw_sides_and_diag(7), test2)
+#> [1] TRUE
+all.equal(draw_sides_and_diag(5), test3)
+#> [1] TRUE
+all.equal(draw_sides_and_diag(4), test4)
+#> [1] TRUE
+

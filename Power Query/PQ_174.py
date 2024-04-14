71d5e882074e8d2d2c7458f3b5da0bd8ab6de9cf
kgryczan <kgryczan@gmail.com>
2024-04-14 11:04:25 +0200

    Refactor Power Query script to include date padding

--- Power Query/PQ_174.py
+++ Power Query/PQ_174.py
@@ -5,15 +5,15 @@ input = pd.read_excel("PQ_Challenge_174.xlsx", sheet_name="Sheet1",  usecols="A:
 test = pd.read_excel("PQ_Challenge_174.xlsx", sheet_name="Sheet1",  usecols="F:J", nrows=20)
 test.columns = ["Emp", "From Date", "To Date", "Monthly Sales", "Running Total"]
 
-result = input.melt(id_vars=["Emp", "Sales"], var_name="date", value_name="value").sort_values(["Emp", "value"]).reset_index(drop=True)
-
+# function mimicing R padr::pad() function to fill missing dates
 def pad(df, date_col, freq='D'):
     df[date_col] = pd.to_datetime(df[date_col])
     df = df.set_index(date_col)
     df = df.asfreq(freq)
     df = df.reset_index()
     return df
-    
+
+result = input.melt(id_vars=["Emp", "Sales"], var_name="date", value_name="value").sort_values(["Emp", "value"]).reset_index(drop=True)    
 result = result.groupby("Emp").apply(lambda x: pad(x, "value"))
 result = result.fillna(method='ffill').reset_index(drop=True)
 result["days"] = result.groupby("Emp")["value"].transform("count")

58db338e765dcb76a7315af1da5d540066b25556
kgryczan <kgryczan@gmail.com>
2024-06-09 10:56:20 +0200

    PQ_190 Uploaded

---
+++ Power Query/PQ_Challenge_190.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(readxl)
+library(rebus)
+
+input = read_excel("Power Query/PQ_Challenge_190.xlsx", range = "A1:A3")
+test  = read_excel("Power Query/PQ_Challenge_190.xlsx", range = "A6:E8")
+
+name_pattern = "Name:" %R% capture(one_or_more(WRD)) %R% "Org:"
+org_pattern = "Org:" %R% capture(one_or_more(WRD)) %R% "City:"
+city_pattern = "City:" %R% capture(one_or_more(WRD)) %R% "FromDate:"
+from_date_pattern = "FromDate:" %R% capture(one_or_more(WRD)) %R% "ToDate:"
+to_date_pattern = "ToDate:" %R% capture(one_or_more(WRD))
+
+extract_and_space <- function(a, name_pattern) {
+  extracted <- str_match(a, name_pattern)
+  result <- extracted %>% 
+    pluck(2) %>%
+    {if (is.na(.)) extracted %>% pluck(1) else .} %>%
+    str_replace_all("([a-z])([A-Z])", "\\1 \\2") %>%
+    str_replace_all("([A-Z])([A-Z][a-z])", "\\1 \\2")
+  
+  return(result)
+}
+
+result = input %>%
+  mutate(Name = map_chr(Data, ~extract_and_space(.x, name_pattern)),
+         Org = map_chr(Data, ~ str_match(.x, org_pattern) %>% pluck(2)),
+         City = map_chr(Data, ~ extract_and_space(.x, city_pattern)),
+         `From Date` = map_chr(Data, ~ str_match(.x, from_date_pattern) %>% pluck(2)),
+         `To Date` = map_chr(Data, ~ str_match(.x, to_date_pattern) %>% pluck(2))) %>%
+  mutate(`From Date` = ymd(`From Date`) %>% as.POSIXct(),
+         `To Date` = ymd(`To Date`) %>% as.POSIXct()) %>%
+  select(-Data)
+
+identical(result, test)
+# [1] TRUE         

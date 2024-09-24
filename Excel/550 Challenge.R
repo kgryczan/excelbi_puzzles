34afbf511db531f7c9ba00380c38f5a6047a8630
kgryczan <kgryczan@gmail.com>
2024-09-24 12:24:24 +0200

    Exc 550 Uploaded

---
+++ Excel/550 Challenge.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(readxl)
+
+path = "Excel/550 Pick the Odd Numbers in a Grid.xlsx"
+input1 = read_excel(path, range = "A2:B3", col_names = FALSE) %>% as.matrix()
+test1  = read_excel(path, range = "G2:G2", col_names = FALSE) %>% pull()
+
+input2 = read_excel(path, range = "A5:C7", col_names = FALSE) %>% as.matrix()
+test2  = read_excel(path, range = "G5:G5", col_names = FALSE) %>% pull()
+
+input3 = read_excel(path, range = "A9:C11", col_names = FALSE) %>% as.matrix()
+test3  = read_excel(path, range = "G9:G9", col_names = FALSE) %>% pull()
+
+input4 = read_excel(path, range = "A13:D16", col_names = FALSE) %>% as.matrix()
+test4  = read_excel(path, range = "G13:G13", col_names = FALSE) %>% pull()
+
+input5 = read_excel(path, range = "A18:E22", col_names = FALSE) %>% as.matrix()
+test5  = read_excel(path, range = "G18:G18", col_names = FALSE) %>% pull()
+
+pick_odds <- function(M) {
+  all <- as.numeric(apply(rbind(M, t(M), M[,nrow(M):1],t(M)[,nrow(t(M)):1]) , 1, paste0, collapse = ""))
+  paste(all[all %% 2 == 1], collapse = ", ")
+}
+
+all.equal(pick_odds(input1), test1) # 54 shouldn't be here
+all.equal(pick_odds(input2), test2) # TRUE
+all.equal(pick_odds(input3), test3) # only discrepancy is 011 vs 11
+all.equal(pick_odds(input4), test4) # TRUE
+all.equal(pick_odds(input5), test5) # TRUE
